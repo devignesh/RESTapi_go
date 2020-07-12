3738f4792890f811ad992e7fe1a4cd1b1be4646c
devignesh <vinesh1865@gmail.com>
2020-07-12 23:17:31 +0530

    adding orm

--- goORM/user.go
+++ goORM/user.go
@@ -19,7 +19,7 @@ type User struct {
 }
 
 func IntialMigration() {
-	db, err = gorm.Open("sqllite3", "test.db")
+	db, err = gorm.Open("sqlite3", "test.db")
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("failed to connect")
