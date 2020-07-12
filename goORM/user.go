3173d0f66b8616d930de7e7c1a794fe524fa12d3
devignesh <vinesh1865@gmail.com>
2020-07-12 21:55:10 +0530

    adding function

--- goORM/user.go
+++ goORM/user.go
@@ -8,3 +8,15 @@ import (
 func Allusers(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(w, "All users end point")
 }
+
+func NewUser(w http.ResposeWriter, r *http.Request) {
+	fmt.Println(w, "new user end point")
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Delete user end point")
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "update user end point")
+}
