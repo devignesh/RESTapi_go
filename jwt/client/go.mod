dae98ee2065d26f5037c2ba9401ca98655b58c80
devignesh <vinesh1865@gmail.com>
2020-07-14 00:09:32 +0530

    adding jwt snippets

--- jwt/client/go.mod
+++ jwt/client/go.mod
@@ -1,3 +1,5 @@
 module github.com/devignesh/RESTapi_go/jwt/client
 
 go 1.12
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
--- jwt/client/main.go
+++ jwt/client/main.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	// jwt "github.com/dgrijava/jwt-go"
 )
+
+var mySigningKey = []byte("mysupersecretphrase")
+
+func GenerateJwt() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	cliams := token.Claims.(jwt.MapClaims)
+
+	cliams["authorized"] = true
+	cliams["user"] = "vignesh"
+	cliams["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+	tokenString, err := token.SignedString(mySigningKey)
+	if err != nil {
+		fmt.Errorf("something went wrong: %s", err.Error())
+		return "", err
+	}
+
+	return tokenString, nil
+}
 
 func main() {
 	fmt.Println("simple client")
+
+	tokenString, err := GenerateJwt()
+
+	if err != nil {
+		fmt.Println("erroe while genereaating token")
+	}
+	fmt.Println(tokenString)
 }
