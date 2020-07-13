bc86c40aca4f9031b5a77ef4871c3b45c8a2a843
devignesh <vinesh1865@gmail.com>
2020-07-14 01:17:17 +0530

    adding clinet

--- jwt/client/main.go
+++ jwt/client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -17,7 +18,23 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
-	fmt.Println(w, validtoken)
+	// fmt.Println(w, validtoken)
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://localhost:9000/", nil)
+	req.Header.Set("Token", validtoken)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s", err.Error())
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+
+	}
+
+	fmt.Fprintf(w, string(body))
 }
 
 func GenerateJwt() (string, error) {
