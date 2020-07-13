f98611bdcdfd4f353faed23b529de73cbb10414f
devignesh <vinesh1865@gmail.com>
2020-07-13 22:50:10 +0530

    adding addition

--- gowebassembly/main.go
+++ gowebassembly/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
+
+func add(i []js.Value) {
+	js.Global().Set("output", js.ValueOf(i[0].Int()+i[1].Int()))
+	println(js.ValueOf(i[0].Int() + i[1].Int()).String())
+}
+
+func registerallbacks() {
+	js.Global().Set("add", js.NewCallback(add))
+}
 
 func main() {
 	fmt.Println("hii hello")
+
+	c := make(chan struct{}, 0)
+
+	registerallbacks()
 }
