336d9dd13987f9820dcc29d5916eb459bbf13c82
devignesh <vinesh1865@gmail.com>
2020-07-13 23:26:49 +0530

    adding addition

--- gowebassembly/main.go
+++ gowebassembly/main.go
@@ -10,8 +10,26 @@ func add(i []js.Value) {
 	println(js.ValueOf(i[0].Int() + i[1].Int()).String())
 }
 
+func substract(i []js.Value) {
+	js.Global().Set("output", js.ValueOf(i[0].Int()-i[1].Int()))
+	println(js.ValueOf(i[0].Int() - i[1].Int()).String())
+}
+
+func multiply(i []js.Value) {
+	js.Global().Set("output", js.ValueOf(i[0].Int()*i[1].Int()))
+	println(js.ValueOf(i[0].Int() * i[1].Int()).String())
+}
+
+func division(i []js.Value) {
+	js.Global().Set("output", js.ValueOf(i[0].Int()/i[1].Int()))
+	println(js.ValueOf(i[0].Int() / i[1].Int()).String())
+}
+
 func registerallbacks() {
 	js.Global().Set("add", js.NewCallback(add))
+	js.Global().Set("subtract", js.NewCallback(substract))
+	js.Global().Set("multiply", js.NewCallback(multiply))
+	js.Global().Set("division", js.NewCallback(division))
 }
 
 func main() {
