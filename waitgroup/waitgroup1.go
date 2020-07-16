4868e18b439d61ad194c1d12ff14d577433b7149
devignesh <vinesh1865@gmail.com>
2020-07-17 01:06:38 +0530

    waitgroup tutorial

--- waitgroup/waitgroup1.go
+++ waitgroup/waitgroup1.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
+
+func waitgp(wg *sync.WaitGroup) {
+	time.Sleep(1 * time.Second)
+	fmt.Println("finish goroute")
+	wg.Done()
+
+}
 
 func main() {
 	fmt.Println("waitgroup tutorial")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go waitgp(&wg)
+	wg.Wait()
+	fmt.Println("finish execution of waitgroup")
 
 }
