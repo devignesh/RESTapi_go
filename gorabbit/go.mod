f6a36cecbeaccc220a316fb0605afe673258336f
devignesh <vinesh1865@gmail.com>
2020-07-26 23:50:37 +0530

    connceting go with rbmq

--- gorabbit/go.mod
+++ gorabbit/go.mod
@@ -1,3 +1,5 @@
 module RESTapi_go/gorabbit
 
 go 1.12
+
+require github.com/streadway/amqp v1.0.0 // indirect
--- gorabbit/main.go
+++ gorabbit/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/streadway/amqp"
 )
 
 func main() {
 	fmt.Println("go rabbitMQ")
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Successfully connected")
 }
