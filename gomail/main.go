978d72a85edc5c89be5dcd553c0b0cdb2316e1ec
devignesh <vinesh1865@gmail.com>
2020-07-29 23:21:15 +0530

    go with mail

--- gomail/main.go
+++ gomail/main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/mailgun/mailgun-go"
 )
 
+func SendSimpleMessage(domain, apiKey string) (string, error) {
+	mg := mailgun.NewMailgun("gopherhut.com", "pubkey-ecc733d482a41f1a5aa11d0fb6698ebb")
+	m := mg.NewMessage(
+		"Mailgun Sandbox <postmaster@sandbox80d9ed4a4bae4afab1cc80ea5117d881.mailgun.org>",
+		"Hello Vignesh",
+		"Testing some Mailgun awesomeness!",
+		"vinesh1865@gmail.com",
+	)
+	_, id, err := mg.Send(m)
+	return id, err
+}
+
 func main() {
 	fmt.Println("Go with mail")
+	SendSimpleMessage("gopherhut.com", "pubkey-ecc733d482a41f1a5aa11d0fb6698ebb")
 }
