af1bae6eded8c7a182c3af389a416ccc97f1c0db
devignesh <vinesh1865@gmail.com>
2020-08-02 22:37:21 +0530

    go with reddis

--- reddis/main.go
+++ reddis/main.go
@@ -3,8 +3,15 @@ package main
 import (
 	"fmt"
 
+	"encoding/json"
+
 	"github.com/go-redis/redis"
 )
+
+type Author struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
 
 func main() {
 	fmt.Println("GO with reddis")
@@ -17,4 +24,19 @@ func main() {
 
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
+
+	json, err := json.Marshal(Author{Name: "Elliot", Age: 25})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = client.Set("id1234", json, 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+	val, err := client.Get("id1234").Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(val)
 }
