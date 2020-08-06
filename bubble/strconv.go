313f123fb7138e17875dab74d4e8932ad95106d1
devignesh <vinesh1865@gmail.com>
2020-08-06 21:27:16 +0530

    adding func

--- bubble/strconv.go
+++ bubble/strconv.go
@@ -4,7 +4,15 @@ import (
 	"fmt"
 )
 
+func strtoint() {
+	fmt.Println("String to integer conversion")
+}
+
+func inttostr() {
+	fmt.Println("integer to string conversion")
+}
 
 func main() {
 	fmt.Println("String type conversion")
+
 }
