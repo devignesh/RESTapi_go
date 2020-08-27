78815d6c3c7a3e920ae32de6b7d4706d7e1969f5
devignesh <vinesh1865@gmail.com>
2020-08-27 23:01:17 +0530

    char types

---
+++ kloudone/table/char.sql
@@ -0,0 +1,9 @@
+create table chartype (
+id serial primary key, 
+name char(255),
+job varchar(15),
+company text
+);
+
+
+insert into chartype (id, name, job, company) values (1,'vicky', 'SDE1', 'Deftouch');
