b87053c252a6ced017f97ea1c526927c7de95890
devignesh <vinesh1865@gmail.com>
2020-08-28 00:33:55 +0530

    date

--- kloudone/table/date.sql
+++ kloudone/table/date.sql
@@ -13,6 +13,16 @@ create table emp_datetypes(
     joined_date date default current_date
 );
 
+
+create table dt_test (
+    start_date date, 
+    start_time time, 
+    completion_time timestamp, 
+    completion_date timestamptz, 
+    no_of_year interval
+    );
+    
+
 select current_date;
 select_now()::date;
 select to_char(now() :: date, 'dd/mm/yyyy');
