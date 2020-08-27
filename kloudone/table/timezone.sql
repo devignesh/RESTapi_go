b0f48d820759b1c5d1c947e89012775c5bd0f642
devignesh <vinesh1865@gmail.com>
2020-08-28 00:08:32 +0530

    timezones

---
+++ kloudone/table/timezone.sql
@@ -0,0 +1,12 @@
+create table timestamp(
+    ts timestamp,
+    timestapwithtimezone timestamptz,
+    timestampwithouttimezzone timestampwithouttimezzone,
+    timess time,
+    timewithtime timewithtime,
+
+);
+
+set timezone = 'America/Los_Angeles';
+show timezone;
+
