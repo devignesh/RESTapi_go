78dab06477ac4d57a91d6ef9f98c196ebd09963a
devignesh <vinesh1865@gmail.com>
2020-08-28 14:16:35 +0530

    adding more table fileds

--- kloudone/table/table.sql
+++ kloudone/table/table.sql
@@ -3,3 +3,11 @@ create table deptt(
     dept_name text NOT NULL,  
     Location varchar(15)
 );
+
+
+create table student(                                                      
+    Stu_id int, 
+    Stu_Name text, 
+    Stu_Age int, 
+    Stu_address char(30)
+);
