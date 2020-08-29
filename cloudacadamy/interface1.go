fec1e4f7103d38bc005fe7fd3df7f4b9e570c7bd
devignesh <vinesh1865@gmail.com>
2020-08-29 23:16:41 +0530

    interface with pointer

--- cloudacadamy/interface1.go
+++ cloudacadamy/interface1.go
@@ -1,19 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type device interface {
 	turnOn() string
+	update(version float32)
 }
 
 type mobile struct {
-	name  string
-	model string
+	name    string
+	model   string
+	version float32
 }
 
 type laptop struct {
-	name  string
-	model string
+	name    string
+	model   string
+	version float32
 }
 
 func (phone mobile) turnOn() string {
@@ -24,14 +30,30 @@ func (lap laptop) turnOn() string {
 	return "Laptop starting up..."
 }
 
-func main() {
-	dev1 := mobile{"oneplus", "8 Pro"}
-	dev2 := laptop{"mackbook pro", "1.79l"}
+func (ph *mobile) update(version float32) {
+	ph.version = version
+}
 
-	devices := []device{dev1, dev2}
+func (laptops *laptop) update(version float32) {
+	laptops.version = version
+}
+
+func main() {
+	dev1 := mobile{"oneplus", "8 Pro", 1.0}
+	dev2 := laptop{"mackbook pro", "1.79l", 8.1}
+
+	devices := []device{&dev1, &dev2}
 	fmt.Println(devices)
 
 	for _, dev := range devices {
+		if strings.Contains(dev.turnOn(), "Oneplus") {
+			dev.update(1.1)
+		} else if strings.Contains(dev.turnOn(), "mackbook") {
+			dev.update(8.12)
+		}
 		fmt.Println(dev.turnOn())
 	}
+
+	fmt.Println(dev1)
+	fmt.Println(dev2)
 }
