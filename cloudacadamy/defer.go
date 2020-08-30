2baa999cc2e8061f3e8e56cce6e0cf51f2a3852e
devignesh <vinesh1865@gmail.com>
2020-08-30 21:18:38 +0530

    defer with go

---
+++ cloudacadamy/defer.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func doSomething(msg string) {
+	fmt.Println(msg)
+}
+
+func system() int {
+	fmt.Println("system started...")
+
+	defer doSomething("cleanup")
+	defer doSomething("stop")
+
+	fmt.Println("system finished!")
+
+	return 1
+}
+
+func main() {
+	data := system()
+	fmt.Println(data)
+}
