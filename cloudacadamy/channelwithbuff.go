90e2a131339904eb400dcedca78e10ae15f09972
devignesh <vinesh1865@gmail.com>
2020-08-31 22:07:14 +0530

    channels with buffers

---
+++ cloudacadamy/channelwithbuff.go
@@ -0,0 +1,26 @@
+package main
+
+import "time"
+
+func main() {
+	size := 3
+	var buffChan = make(chan int, size)
+
+	//reader
+	go func() {
+		for {
+			_ = <-buffChan
+			time.Sleep(time.Second)
+		}
+	}()
+
+	//writer
+	writer := func() {
+		for i := 1; i <= 10; i++ {
+			buffChan <- i
+			println(i)
+		}
+	}
+
+	writer()
+}
