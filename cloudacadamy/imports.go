dd102c2a6b183640d8f5c6626588ec0c0d945e7a
devignesh <vinesh1865@gmail.com>
2020-09-01 22:20:42 +0530

    go

--- cloudacadamy/imports.go
+++ cloudacadamy/imports.go
@@ -12,8 +12,10 @@ import m "math"
 
 func main() {
 	name := "vicky"
+	vr := "Gopher"
 
 	fmt.Println(strings.ToUpper(name))
+	fmt.Println(vr)
 	fmt.Println(uuid.New())
 	f, _ := strconv.ParseFloat("3.1415", 64)
 	fmt.Println(f)
