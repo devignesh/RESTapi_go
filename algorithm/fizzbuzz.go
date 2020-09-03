4e1f41fa4d2225bd5d7d4a61a0b9167b64783dce
devignesh <vinesh1865@gmail.com>
2020-09-04 00:18:04 +0530

    fizzbuzz pgm

---
+++ algorithm/fizzbuzz.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	for i := 0; i <= 20; i++ {
+		if i %15 == 0 {
+			fmt.Println("fizzbuzz")
+		} else if i % 3 == 0 {
+			fmt.Println("fizz")
+		} else if i % 5 == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
