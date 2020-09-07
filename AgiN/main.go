186b3bb8eb190745874dfdf2157173ba3231d424
devignesh <vinesh1865@gmail.com>
2020-09-07 20:13:27 +0530

    initial commit

---
+++ AgiN/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"msg": "vicky",
+		})
+	})
+
+	r.Run()
+}
