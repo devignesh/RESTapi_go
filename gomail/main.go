b191718a5c76bacd3f0853333bef93c5725ac352
devignesh <vinesh1865@gmail.com>
2020-09-12 23:06:41 +0530

    gomail

--- gomail/main.go
+++ gomail/main.go
@@ -10,7 +10,7 @@ func SendSimpleMessage(domain, apiKey string) (string, error) {
 	mg := mailgun.NewMailgun("gopherhut.com", "pubkey-ecc733d482a41f1a5aa11d0fb6698ebb")
 	m := mg.NewMessage(
 		"Mailgun Sandbox <postmaster@sandbox80d9ed4a4bae4afab1cc80ea5117d881.mailgun.org>",
-		"Hello Vignesh",
+		"Hello Vignesh kumar",
 		"Testing mail with go",
 		"vigneshkumar.mca2016@adhiyamaan.in",
 	)
