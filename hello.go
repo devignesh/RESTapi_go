fa83c28624b78b326f43be78b904120b0ee664bb
devignesh <vinesh1865@gmail.com>
2020-09-13 23:00:56 +0530

    go stuff

--- hello.go
+++ hello.go
@@ -12,7 +12,7 @@ const defaultAddr = ":8080"
 // main starts an http server on the $PORT environment variable.
 func main() {
 	addr := defaultAddr
-	// $PORT environment variable is provided in the Kubernetes deployment.
+	
 	if p := os.Getenv("PORT"); p != "" {
 		addr = ":" + p
 	}
@@ -24,7 +24,7 @@ func main() {
 	}
 }
 
-// home logs the received request and returns a simple response.
+
 func home(w http.ResponseWriter, r *http.Request) {
 	log.Printf("received request: %s %s", r.Method, r.URL.Path)
 	fmt.Fprintf(w, "Hello, world!")
