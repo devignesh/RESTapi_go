6666428f76974aaa559c25e014a732a41d09b011
devignesh <vinesh1865@gmail.com>
2020-09-15 21:58:24 +0530

    adding table

--- kloudone/pglogical/citusdata.sql
+++ kloudone/pglogical/citusdata.sql
@@ -32,3 +32,71 @@ SELECT * from master_add_node('{connection ip or localhost}', {portnumber});
 select * from master_get_active_worker_nodes();
 
 
+-- Coordinate with master node -- 1111
+
+CREATE TABLE companies (
+    
+    id bigint NOT NULL,
+    
+    name text NOT NULL,
+    
+    image_url text,
+    
+    created_at timestamp without time zone NOT NULL,
+    
+    updated_at timestamp without time zone NOT NULL
+);
+
+CREATE TABLE campaigns (
+    
+    id bigint NOT NULL,
+    
+    company_id bigint NOT NULL,
+    
+    name text NOT NULL,
+    
+    cost_model text NOT NULL,
+    
+    state text NOT NULL,
+    
+    monthly_budget bigint,
+    
+    blacklisted_site_urls text[],
+    
+    created_at timestamp without time zone NOT NULL,
+    
+    updated_at timestamp without time zone NOT NULL
+);
+
+CREATE TABLE ads (
+    
+    id bigint NOT NULL,
+    
+    company_id bigint NOT NULL,
+    
+    campaign_id bigint NOT NULL,
+    
+    name text NOT NULL,
+    
+    image_url text,
+    
+    target_url text,
+    
+    impressions_count bigint DEFAULT 0,
+    
+    clicks_count bigint DEFAULT 0,
+    
+    created_at timestamp without time zone NOT NULL,
+    
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE companies ADD PRIMARY KEY (id);
+
+ALTER TABLE campaigns ADD PRIMARY KEY (id, company_id);
+
+ALTER TABLE ads ADD PRIMARY KEY (id, company_id);
+
+
+-- citus replication model :
